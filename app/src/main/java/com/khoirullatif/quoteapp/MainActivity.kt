85e4d8023ccf98a3ef4ae858527c6ee6e1156256
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-07-27 22:55:50 +0700

    menambahkan library Loopj

--- app/src/main/java/com/khoirullatif/quoteapp/MainActivity.kt
+++ app/src/main/java/com/khoirullatif/quoteapp/MainActivity.kt
@@ -2,10 +2,52 @@ package com.khoirullatif.quoteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import com.khoirullatif.quoteapp.databinding.ActivityMainBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getRandomQuote()
+    }
+
+    private fun getRandomQuote() {
+        binding.progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://quote-api.dicoding.dev/random"
+
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
